b80fa4ca4da83f4ccf406f6c871cbcf39f582fa6
Charles Xu <charlesxu@caltech.edu>
2024-08-28 18:58:56 -0700

    installenv

--- install_env.py
+++ install_env.py
@@ -6,13 +6,19 @@ env_file = 'environment.yml'
 with open(env_file, 'r') as f:
     env = yaml.safe_load(f)
 
-def create_conda_env(env_file):
-    try:
-        # Create the conda environment
-        subprocess.run(f"conda create --name {env['name']} python={env['dependencies'][0]}", shell=True)
-        print('Conda environment created successfully!')
-    except subprocess.CalledProcessError as e:
-        print(f'Error creating conda environment: {e}')
+# print(next((s for s in env['dependencies'] if s.startswith('python=')), None))
 
-# Call the function to create the conda environment
-create_conda_env(env_file)
+# try:
+#     # Create the conda environment
+#     subprocess.run(f"conda create --name {env['name']} {next((s for s in env['dependencies'] if s.startswith('python=')), None)}", shell=True)
+#     print('Conda environment created successfully!')
+# except subprocess.CalledProcessError as e:
+#     print(f'Error creating conda environment: {e}')
+
+for dep in env['dependencies'][1:]:
+    try:
+        # Install the dependencies
+        subprocess.run(f"conda install --name {env['name']} {dep} -y", shell=True, check=True)
+        print(f'{dep} installed successfully!')
+    except subprocess.CalledProcessError as e:
+        print(f'Error installing {dep}: {e}')
